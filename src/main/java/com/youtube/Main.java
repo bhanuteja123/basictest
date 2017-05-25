49025f1e327e8af56ca5137f7a7e54a3a30d0041
bhanuteja123 <bhanuteja1993@gmail.com>
2017-05-25 11:31:36 -0400

    Commit 6

--- src/main/java/com/youtube/Main.java
+++ src/main/java/com/youtube/Main.java
@@ -12,10 +12,8 @@ public class Main {
         driver.get("https://www.youtube.com/");
         driver.manage().window().maximize();
         driver.findElement(By.xpath("//*[@id=\"yt-masthead-signin\"]/div/button")).click();
-        driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("surampudit@gmail.com");
         driver.findElement(By.id("identifierNext")).click();
         Thread.sleep(5000);
-        driver.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input")).sendKeys("Bhanu@17");
         driver.findElement(By.id("passwordNext")).click();
         Thread.sleep(3000);
         driver.findElement(By.xpath("//*[@id=\"VLWL-guide-item\"]/a")).click();
@@ -26,8 +24,6 @@ public class Main {
         Thread.sleep(3000);
         driver.findElement(By.xpath("//div[@id=':6']/div")).click();
         System.out.println("args 1");
-        driver.findElement(By.xpath("//div[@id=':7']/div")).click();
-
-
+        driver.findElement(By.xpath("//div[@id=':7']")).click();
     }
 }
